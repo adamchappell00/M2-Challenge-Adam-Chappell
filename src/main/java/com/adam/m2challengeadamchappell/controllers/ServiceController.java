63b9a6316fd5c23d0c90ab11af77886b78770a15
adamchappell00 <adam.chappell00@gmail.com>
2022-05-11 09:34:39 -0500

    Added jUnit Dependency

--- src/main/java/com/adam/m2challengeadamchappell/controllers/ServiceController.java
+++ src/main/java/com/adam/m2challengeadamchappell/controllers/ServiceController.java
@@ -1,9 +1,49 @@
 package com.adam.m2challengeadamchappell.controllers;
 
+// Project Imports
+import com.adam.m2challengeadamchappell.models.Month;
+
+// Java Imports
+import java.util.ArrayList;
+import java.util.List;
 
 // Spring Imports
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ServiceController {
+
+    private List<Month> allMonths;
+
+    public ServiceController(){
+        allMonths = new ArrayList<>();
+        allMonths.add(new Month(1, "January"));
+        allMonths.add(new Month(2, "February"));
+        allMonths.add(new Month(3, "March"));
+        allMonths.add(new Month(4, "April"));
+        allMonths.add(new Month(5, "May"));
+        allMonths.add(new Month(6, "June"));
+        allMonths.add(new Month(7, "July"));
+        allMonths.add(new Month(8, "August"));
+        allMonths.add(new Month(9, "September"));
+        allMonths.add(new Month(10, "October"));
+        allMonths.add(new Month(11, "November"));
+        allMonths.add(new Month(12, "December"));
+    }
+
+    @RequestMapping(value="/month/{monthNumber}", method= RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public Month getMonthByNumber(@PathVariable int monthNumber) {
+
+        // Range Check for Exceptions
+        if(monthNumber >= 1 && monthNumber <= 12){
+            // Shift from Month Number to Index
+            return allMonths.get(monthNumber - 1);
+        } else {
+            throw new IllegalArgumentException("You Must Request a Month between 1 and 12");
+        }
+    }
+
+
 }
